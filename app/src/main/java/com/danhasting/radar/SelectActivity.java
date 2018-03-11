256e542d87102051eebfb59a4aac1cf3dd6bc4ef
Dan Hasting <commits@hasting.email>
2018-03-11 14:52:05 -0400

    Remember form selections

--- app/src/main/java/com/danhasting/radar/SelectActivity.java
+++ app/src/main/java/com/danhasting/radar/SelectActivity.java
@@ -2,12 +2,15 @@ package com.danhasting.radar;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Switch;
+
+import java.util.Arrays;
 
 public class SelectActivity extends MainActivity {
 
@@ -25,25 +28,43 @@ public class SelectActivity extends MainActivity {
                 this, R.array.type_names, android.R.layout.simple_spinner_dropdown_item);
         typeSpinner.setAdapter(typeAdapter);
 
+        String type = settings.getString("last_type","");
+        int index = Arrays.asList(getResources().getStringArray(R.array.type_values)).indexOf(type);
+        typeSpinner.setSelection(index);
+
         Spinner locationSpinner = findViewById(R.id.locationSpinner);
         ArrayAdapter<CharSequence> locationAdapter = ArrayAdapter.createFromResource(
                 this, R.array.location_names, android.R.layout.simple_spinner_dropdown_item);
         locationSpinner.setAdapter(locationAdapter);
+
+        String location = settings.getString("last_location","");
+        index = Arrays.asList(getResources().getStringArray(R.array.location_values)).indexOf(location);
+        locationSpinner.setSelection(index);
+
+        Switch loopSwitch = findViewById(R.id.loopSwitch);
+        loopSwitch.setChecked(settings.getBoolean("last_loop",false));
     }
 
     public void viewRadar(View v) {
         Intent radarIntent = new Intent(SelectActivity.this, RadarActivity.class);
 
         Spinner typeSpinner = findViewById(R.id.typeSpinner);
-        radarIntent.putExtra("type",
-                getResources().getStringArray(R.array.type_values)[typeSpinner.getSelectedItemPosition()]);
-
         Spinner locationSpinner = findViewById(R.id.locationSpinner);
-        radarIntent.putExtra("location",
-                getResources().getStringArray(R.array.location_values)[locationSpinner.getSelectedItemPosition()]);
-
         Switch loopSwitch = findViewById(R.id.loopSwitch);
-        radarIntent.putExtra("loop", loopSwitch.isChecked());
+
+        String location = getResources().getStringArray(R.array.location_values)[locationSpinner.getSelectedItemPosition()];
+        String type = getResources().getStringArray(R.array.type_values)[typeSpinner.getSelectedItemPosition()];
+        Boolean loop = loopSwitch.isChecked();
+
+        radarIntent.putExtra("type", type);
+        radarIntent.putExtra("location", location);
+        radarIntent.putExtra("loop", loop);
+
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("last_location", location);
+        editor.putString("last_type", type);
+        editor.putBoolean("last_loop", loop);
+        editor.apply();
 
         SelectActivity.this.startActivity(radarIntent);
     }
