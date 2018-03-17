b70896eed7b69a8d2e40f35afb80e90d5b426f19
Dan Hasting <commits@hasting.email>
2018-03-16 22:40:14 -0400

    Defocus location input when keyboard is hidden

---
+++ app/src/main/java/com/danhasting/radar/EditTextDefocus.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018, Dan Hasting
+ *
+ * This file is part of WeatherRadar
+ *
+ * WeatherRadar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WeatherRadar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WeatherRadar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.danhasting.radar;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+
+public class EditTextDefocus extends AppCompatEditText {
+
+    public EditTextDefocus(Context context) {
+        super(context);
+    }
+
+    public EditTextDefocus(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EditTextDefocus(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onKeyPreIme(int keyCode, KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK)
+            clearFocus();
+        return super.dispatchKeyEvent(event);
+    }
+}
