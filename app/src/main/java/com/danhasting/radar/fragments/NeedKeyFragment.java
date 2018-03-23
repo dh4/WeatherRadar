c5cd2d02e446af5ae7cad66abc50218eb9002fe7
Dan Hasting <commits@hasting.email>
2018-03-23 16:09:34 -0400

    Remove unused import

--- app/src/main/java/com/danhasting/radar/fragments/NeedKeyFragment.java
+++ app/src/main/java/com/danhasting/radar/fragments/NeedKeyFragment.java
@@ -29,7 +29,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.danhasting.radar.R;
-import com.danhasting.radar.SettingsActivity;
 
 public class NeedKeyFragment extends Fragment {
 
